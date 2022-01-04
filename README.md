a72f166c546aeb3104eece9522d17f01f11d1866
demola malomo <malomo.alaba@yahoo.com>
2022-01-04 23:48:48 +0100

    repo setup and number in list algo

---
+++ README.md
@@ -0,0 +1,3 @@
+# DS_Algos_in_golang
+
+This repository is to track my progress and learning path as I explore Data Structure and Algorithmn using Golang.
---
+++ num_in_list.go
@@ -0,0 +1,10 @@
+package dsalgosingolang
+
+func NumInList(list []int, num int) bool {
+	for _, idx := range list {
+		if idx == num {
+			return true
+		}
+	}
+	return false
+}
